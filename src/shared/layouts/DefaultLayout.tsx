1936cebe60653bbbce83f0925e3fc459d43e4406
qoymu <vlad.nikitienko@gmail.com>
2026-01-14 16:59:33 +0300

    feat: added navigation

--- src/shared/layouts/DefaultLayout.tsx
+++ src/shared/layouts/DefaultLayout.tsx
@@ -1,10 +1,14 @@
-import { Outlet } from '@tanstack/react-router';
+import { Link, Outlet } from '@tanstack/react-router';
 
 import styles from './styles.module.scss';
 
 export const DefaultLayout = () => {
 	return (
 		<main className={styles.container}>
+			<div className={styles.navigation}>
+				<Link to="/home">Home</Link>
+				<Link to="/table">Table</Link>
+			</div>
 			<Outlet />
 		</main>
 	);
